ae8466eac4b91cb26c94c587aec24fef2aeb313c
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-12-21 18:34:32 +0300

    add update and delete cover

--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -3,28 +3,43 @@ import type {
   PlaylistData,
   PlaylistsResponse,
   UpdatePlaylistArgs,
-} from '@/features/playlists/api/playlistsApi.types.ts'
-import {baseApi} from "@/app/api/baseApi.ts";
+} from "@/features/playlists/api/playlistsApi.types.ts"
+import { baseApi } from "@/app/api/baseApi.ts"
+import type { Images } from "@/common/types"
 
 export const playlistsApi = baseApi.injectEndpoints({
-  endpoints: (build) => ({
-    fetchPlaylists: build.query<PlaylistsResponse, void>({
-      query: () => 'playlists',
-      providesTags: ['Playlist'],
-    }),
-    createPlaylist: build.mutation<{ data: PlaylistData }, CreatePlaylistArgs>({
-      query: (body) => ({ method: 'post', url: 'playlists', body }),
-      invalidatesTags: ['Playlist'],
-    }),
-    deletePlaylist: build.mutation<void, string>({
-      query: (playlistId) => ({ method: 'delete', url: `playlists/${playlistId}` }),
-      invalidatesTags: ['Playlist'],
-    }),
-    updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArgs }>({
-      query: ({ playlistId, body }) => ({ method: 'put', url: `playlists/${playlistId}`, body }),
-      invalidatesTags: ['Playlist'],
-    }),
-  }),
+  endpoints: (build) => {
+    return {
+      fetchPlaylists: build.query<PlaylistsResponse, void>({
+        query: () => "playlists",
+        providesTags: ["Playlist"],
+      }),
+      createPlaylist: build.mutation<{ data: PlaylistData }, CreatePlaylistArgs>({
+        query: (body) => ({ method: "post", url: "playlists", body }),
+        invalidatesTags: ["Playlist"],
+      }),
+      deletePlaylist: build.mutation<void, string>({
+        query: (playlistId) => ({ method: "delete", url: `playlists/${playlistId}` }),
+        invalidatesTags: ["Playlist"],
+      }),
+      updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArgs }>({
+        query: ({ playlistId, body }) => ({ method: "put", url: `playlists/${playlistId}`, body }),
+        invalidatesTags: ["Playlist"],
+      }),
+      uploadPlaylistCover: build.mutation<Images, { playlistId: string; file: File }>({
+        query: ({ playlistId, file }) => {
+          const formData = new FormData()
+          formData.append("file", file)
+          return { method: "post", url: `playlists/${playlistId}/images/main`, body: formData }
+        },
+        invalidatesTags: ["Playlist"],
+      }),
+      deletePlaylistCover: build.mutation<void, { playlistId: string }>({
+        query: ({ playlistId }) => ({ method: "delete", url: `playlists/${playlistId}/images/main` }),
+        invalidatesTags: ["Playlist"],
+      }),
+    }
+  },
 })
 
 export const {
@@ -32,4 +47,6 @@ export const {
   useCreatePlaylistMutation,
   useDeletePlaylistMutation,
   useUpdatePlaylistMutation,
+  useUploadPlaylistCoverMutation,
+  useDeletePlaylistCoverMutation,
 } = playlistsApi
