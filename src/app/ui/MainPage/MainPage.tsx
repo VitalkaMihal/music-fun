78445ce96c61dc06fe058f6dd79f97f57227c508
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2025-12-28 19:42:29 +0300

    add loginName

--- src/app/ui/MainPage/MainPage.tsx
+++ src/app/ui/MainPage/MainPage.tsx
@@ -1,7 +1,11 @@
+import { useGetMeQuery } from "@/features/auth/api/authApi.ts"
+
 export const MainPage = () => {
+  const { data } = useGetMeQuery()
   return (
     <div>
       <h1>Main page</h1>
+      <h2>Login: {data?.login}</h2>
     </div>
   )
 }
