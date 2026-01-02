94f8812dcf156f93e1f94f2eed9d3eeaf50d16ba
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2026-01-02 20:34:42 +0300

    add isErrorWithProperty

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { toast } from "react-toastify"
+import { isErrorWithProperty } from "@/common/utils"
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
   tagTypes: ["Playlist"],
-  baseQuery: (args, api, extraOptions) => {
-    return fetchBaseQuery({
+  baseQuery: async (args, api, extraOptions) => {
+    const result = await fetchBaseQuery({
       baseUrl: import.meta.env.VITE_BASE_URL,
       headers: {
         "API-KEY": import.meta.env.VITE_API_KEY,
@@ -14,6 +16,31 @@ export const baseApi = createApi({
         return headers
       },
     })(args, api, extraOptions)
+
+    if (result.error) {
+      switch (result.error.status) {
+        case 404:
+          if (isErrorWithProperty(result.error.data, "error")) {
+            toast(result.error.data.error, { type: "error", theme: "colored" })
+          } else {
+            toast(JSON.stringify(result.error.data), { type: "error", theme: "colored" })
+          }
+          break
+
+        case 429:
+          if (isErrorWithProperty(result.error.data, "message")) {
+            toast(result.error.data.message, { type: "error", theme: "colored" })
+          } else {
+            toast(JSON.stringify(result.error.data), { type: "error", theme: "colored" })
+          }
+          break
+
+        default:
+          toast("Some error occurred", { type: "error", theme: "colored" })
+      }
+    }
+
+    return result
   },
   endpoints: () => ({}),
 })
