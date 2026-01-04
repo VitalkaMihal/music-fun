7b0d17680141d457dc1d2b6c50f3b242e68357fd
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2026-01-04 17:25:50 +0300

    add OAuthRedirect

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,16 +1,18 @@
-import { MainPage } from '@/app/ui/MainPage/MainPage.tsx'
-import { PageNotFound } from '@/common/components'
-import { ProfilePage } from '@/features/auth/ui/ProfilePage/ProfilePage.tsx'
-import { PlaylistsPage } from '@/features/playlists/ui/PlaylistsPage.tsx'
-import { TracksPage } from '@/features/tracks/ui/TracksPage.tsx'
-import { Route, Routes } from 'react-router'
+import { MainPage } from "@/app/ui/MainPage/MainPage.tsx"
+import { PageNotFound } from "@/common/components"
+import { ProfilePage } from "@/features/auth/ui/ProfilePage/ProfilePage.tsx"
+import { PlaylistsPage } from "@/features/playlists/ui/PlaylistsPage.tsx"
+import { TracksPage } from "@/features/tracks/ui/TracksPage.tsx"
+import { Route, Routes } from "react-router"
+import { OAuthCallback } from "@/features/auth/ui/OAuthCallback/OAuthCallback.tsx"
 
 export const Path = {
-  Main: '/',
-  Playlists: '/playlists',
-  Tracks: '/tracks',
-  Profile: '/profile',
-  NotFound: '*',
+  Main: "/",
+  Playlists: "/playlists",
+  Tracks: "/tracks",
+  Profile: "/profile",
+  OAuthRedirect: "/oauth/callback",
+  NotFound: "*",
 } as const
 
 export const Routing = () => (
@@ -19,6 +21,7 @@ export const Routing = () => (
     <Route path={Path.Playlists} element={<PlaylistsPage />} />
     <Route path={Path.Tracks} element={<TracksPage />} />
     <Route path={Path.Profile} element={<ProfilePage />} />
+    <Route path={Path.OAuthRedirect} element={<OAuthCallback />} />
     <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
 )
