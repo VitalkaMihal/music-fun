4c1061056b974ba36e67f2b42edd0d3cc6a19e61
VitalkaMihal <vitalimikhalkevich1986@gmail.com>
2026-01-04 17:00:47 +0300

    add Login

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,28 +1,34 @@
-import { Path } from '@/common/routing'
-import { NavLink } from 'react-router'
-import s from './Header.module.css'
+import { Path } from "@/common/routing"
+import { NavLink } from "react-router"
+import s from "./Header.module.css"
+import { useGetMeQuery } from "@/features/auth/api/authApi.ts"
+import { Login } from "@/features/auth/ui/Login/Login.tsx"
 
 const navItems = [
-  { to: Path.Main, label: 'Main' },
-  { to: Path.Playlists, label: 'Playlists' },
-  { to: Path.Tracks, label: 'Tracks' },
-  { to: Path.Profile, label: 'Profile' },
+  { to: Path.Main, label: "Main" },
+  { to: Path.Playlists, label: "Playlists" },
+  { to: Path.Tracks, label: "Tracks" },
+  { to: Path.Profile, label: "Profile" },
 ]
 
 export const Header = () => {
+  const { data } = useGetMeQuery()
+
   return (
     <header className={s.container}>
       <nav>
         <ul className={s.list}>
           {navItems.map((item) => (
             <li key={item.to}>
-              <NavLink to={item.to} className={({ isActive }) => `link ${isActive ? s.activeLink : ''}`}>
+              <NavLink to={item.to} className={({ isActive }) => `link ${isActive ? s.activeLink : ""}`}>
                 {item.label}
               </NavLink>
             </li>
           ))}
         </ul>
       </nav>
+      {data && data.login}
+      {!data && <Login />}
     </header>
   )
 }
